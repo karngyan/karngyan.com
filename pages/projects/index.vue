6d179bf3728d933eceae23bfb22a3ed41195a92f
Karn <mail@karngyan.com>
2021-06-30 00:24:56 +0530

    tech based filters on projects too

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,17 +8,53 @@
         {{ $t('projects.subtext') }}
       </p>
     </div>
+
+    <div data-aos="zoom-in" class="select-none px-4 items-center justify-center sm:justify-start overflow-hidden flex pt-4">
+      <nav class="flex flex-wrap items-center justify-center flex-row space-x-2 sm:space-x-4" aria-label="Tabs">
+        <button @click="currentTech = tech" :class="{ 'bg-gray-900 text-gray-300': tech === currentTech }" v-for="tech in techs" :key="tech"
+                class="flex text-gray-300 focus:outline-none focus:ring-transparent focus:ring-offset-transparent hover:text-hot-pink px-3 py-2 font-medium text-sm rounded-xl">
+          {{ tech }}
+        </button>
+      </nav>
+    </div>
+
     <div data-aos="zoom-in" class="mt-5 gap-4 mx-4 grid max-w-none lg:grid-cols-3">
-      <ProjectCard class="hover:-rotate-12" v-for="project in projects" :key="project.slug" :project="project" />
+      <ProjectCard class="hover:-rotate-12" v-for="project in projectsByTechs" :key="project.slug" :project="project" />
     </div>
   </div>
 </template>
 
 <script>
+const ALL = 'all'
+
 export default {
   head() {
     return {
       title: `projects -- ${this.$config.name}`
+    }
+  },
+  computed: {
+    techs() {
+      let techs = []
+      this.projects.forEach((project) => {
+        project.tech.split(' ').forEach((tech) => {
+          if (tech.trim() !== "") {
+            techs.push(tech.trim())
+          }
+        })
+      })
+      return [ALL, ...new Set(techs)]
+    },
+    projectsByTechs() {
+      if (this.currentTech === ALL)
+        return this.projects
+      return this.projects.filter(project => project.tech.includes(this.currentTech))
+    }
+  },
+  data() {
+    return {
+      currentTech: ALL,
+      ALL: ALL, // exporting it to template
     }
   },
   async asyncData({ $content }) {
