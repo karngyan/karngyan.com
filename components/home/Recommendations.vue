26192b6330f2285c37f647adbbab992d2133cd4f
Sarthak Gupta <sarthakgupta072@gmail.com>
2021-10-24 12:49:06 +0530

    fix: recommendation pointing to linkedIn homepage incase of no link

--- components/home/Recommendations.vue
+++ components/home/Recommendations.vue
@@ -2,7 +2,12 @@
   <div class="max-w-7xl mx-auto border-l border-r border-dashed border-gray-700 pt-4 pb-6 px-4">
     <div class="max-w-prose text-center mx-auto pb-4">
       <p class="text-xs leading-6 text-indigo-600 font-semibold tracking-wide lowercase">{{ $t('recommendations.subtext') }}</p>
-      <a :href="`https://linkedin.com/in/`" target="_blank" rel="noreferrer" class="mt-2 text-3xl hover:text-hot-pink leading-8 font-extrabold tracking-tight text-gray-500">{{ $t('recommendations.header') }}</a>
+      <div v-if="$config.social.linkedin">
+        <a :href="`https://linkedin.com/in/${$config.social.linkedin}`" target="_blank" rel="noreferrer" class="mt-2 text-3xl hover:text-hot-pink leading-8 font-extrabold tracking-tight text-gray-500">{{ $t('recommendations.header') }}</a>
+      </div>
+      <div v-else>
+        <div class="mt-2 text-3xl hover:text-hot-pink leading-8 font-extrabold tracking-tight text-gray-500">{{ $t('recommendations.header') }}</div>
+      </div>
     </div>
 
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
