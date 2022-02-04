2e7fcc70681d405bd7342288fa2f4b1031a7c16d
Malloy Okon <mokut.okon4@gmail.com>
2022-02-04 20:56:27 +0100

    Update karngyan.config.js

--- karngyan.config.js
+++ karngyan.config.js
@@ -1,20 +1,20 @@
 // Upload files in static/ So any file static/file.pdf -> /file.pdf is accessible at root
 
 export default {
-  name: 'gyan prakash karn',
-  domain: 'template.karngyan.com', // add without https:// , used in meta tags and share urls
-  image: '/images/bigheadkarngyan.png',
-  email: 'mail@karngyan.com',
+  name: 'Malloy',
+  domain: 'mallovelli.netlify.app', // add without https:// , used in meta tags and share urls
+  image: '/images/Bigheadmallovelli.png',
+  email: 'mokut.okon@gmail.com',
   googleAnalyticsV4: {
     enabled: false,
-    id: ''
+    id: 'G-MNDKB1HST9'
   },
   plausibleAnalytics: {
-    enabled: true,
+    enabled: false,
     domain: 'template.karngyan.com',
     trackLocalhost: false,
     // leave it empty if plausible is not self hosted
-    apiHost: 'https://analytics.lookatx.dev' // default: https://plausible.io
+    apiHost: 'https://plausible.io' // default: https://plausible.io
   },
   // enable if you want comments and likes on posts
   // see how it looks on karngyan.com
@@ -22,16 +22,16 @@ export default {
     enabled: false
   },
   social: {
-    github: 'karngyan',
-    linkedin: 'karngyan',
-    facebook: 'karnsometimes',
-    twitter: 'gyankarn',
-    instagram: 'karngyan.dev',
-    codestats: 'karngyan' // https://codestats.net make a profile if you dont already have one.
+    github: 'mallovelli',
+    linkedin: 'Mokutmfon Okon',
+    facebook: 'Malloy Okon',
+    twitter: 'mallovelli',
+    instagram: 'mancshaft_',
+    codestats: 'Mallovelli' // https://codestats.net make a profile if you dont already have one.
   },
   buyMeACoffee: {
     enabled: true,
-    url: 'https://www.buymeacoffee.com/karngyan'
+    url: 'https://www.buymeacoffee.com/mokutokon4g'
   },
   projects: {
     enabled: true,
@@ -41,22 +41,22 @@ export default {
   },
   resume: {
     enabled: true,
-    pdfUrl: '/RESUME_GYAN_KARN_DARK.pdf' // add files in static folder
+    pdfUrl: '/My resume.pdf' // add files in static folder
   },
   uses: {
     enabled: true,
-    meta: [
-      {title: 'OS', value: 'macOS Catalina'},
-      {title: 'Memory', value: '16 GB 2667 MHz DDR4'},
-      {title: 'Keyboard', value: 'Keychron K2 - Gateron Brown Keys'},
-      {title: 'Mouse', value: 'Logitech Silent Pebble'},
-      {title: 'Monitor', value: 'LG QHD (2560 x 1440) 27 Inch IPS Display'},
-      {title: 'Laptop • Processor • Graphics', value: 'MacBook Pro (16-inch, 2019) • 2.6 GHz 6-Core Intel Core i7 • AMD Radeon Pro 5300M 4 GB + Intel UHD Graphics 630 1536 MB'}
-    ]
+    //meta: [
+      //{title: 'OS', value: 'Windows 10 pro'},
+      //{title: 'Memory', value: '16 GB 2667 MHz DDR4'},
+      //{title: 'Keyboard', value: 'Keychron K2 - Gateron Brown Keys'},
+      //{title: 'Mouse', value: 'Logitech Silent Pebble'},
+      //{title: 'Monitor', value: 'LG QHD (2560 x 1440) 27 Inch IPS Display'},
+      //{title: 'Laptop • Processor • Graphics', value: 'MacBook Pro (16-inch, 2019) • 2.6 GHz 6-Core Intel Core i7 • AMD Radeon Pro 5300M 4 GB + Intel UHD Graphics 630 1536 MB'}
+    //]
   },
   workedAt: {
     // add logos in static and at max add 3/4
-    enabled: true,
+    enabled: false,
     meta: [
       { name: 'SendPost', src: '/images/sendpost.png', url: 'https://sendpost.io' },
       { name: 'Amazon', src: '/images/amazon.png', url: 'https://amazon.in' },
@@ -65,14 +65,14 @@ export default {
     ]
   },
   recommendations: {
-    enabled: true,
+    enabled: false,
     meta: [
       { name: 'Varun Jain', designation: 'Founder, SendX & SendPost', image: '/images/varun.jpeg', linkedin: 'https://www.linkedin.com/in/varun-jain-582b0215/', content: 'Gyan interned with SendX and SendPost for around 6 months. He is a very strong full-stack engineer who can create pixel perfect frontend while being able to work on backend pieces sending millions of emails per day. He is a quick learner, a great team player and, has a very strong work ethic. You can rely on him that things will be done with utmost quality. He also has pretty good product instincts and can think from the user\'s perspective.' },
       { name: 'Gaurav Sen', designation: 'Founder, InterviewReady', image: '/images/gaurav.jpeg', linkedin: 'https://www.linkedin.com/in/gkcs/', content: 'Karn is an extraordinarily talented, diligent and foresighted individual. He is gifted at building products from paper to code, and has a knack of finding efficient solutions to complex problems. In my experience, I have rarely seen such a mixture of talent and passion put together. At InterviewReady, Karn exceeded our expectations. If you are considering working with him: congratulations, you are going to have a great time!' },
     ]
   },
   loadingIndicator: {
-    name: 'pulse'
+    name: 'rotating-plane'
     // https://tobiasahlin.com/spinkit/
     // circle
     // cube-grid
@@ -96,15 +96,15 @@ export default {
         uses: 'uses',
         resume: 'résumé',
         buyMeACoffee: 'buy me a beer',
-        signIn: 'sign in',
-        signOut: 'sign out'
+        //signIn: 'sign in',
+        //signOut: 'sign out'
       },
       hero: {
-        iBlogTech: 'i blog tech',
+        iBlogTech: 'I write about my travels',
         haveALook: 'have a look',
-        friendlyNeighborhood: 'your friendly neighborhood',
-        description: 'i am a software engineer and a part time freelancer. i blog tech, write a weekend newsletter called software shots, and tinker with side projects every now n then.',
-        words: ['developer', 'designer', 'engineer', 'programmer', 'encoder'],
+        friendlyNeighborhood: 'your not so friendly neighbourhood',
+        description: 'I am a Front End developer, UX designer specializing in Graphics/Visual Design and a part time freelancer. I write a few articles about my interests monthly, and tinker with side projects every now and then.',
+        words: ['developer', 'designer', 'engineer', 'programmer', 'writer', 'photographer'],
       },
       githubCalendar: {
         header: 'contributions',
@@ -112,7 +112,7 @@ export default {
       },
       blog: {
         header: 'blog',
-        subtext: 'i try to write once in a while. let me know your thoughts in comments or mail@karngyan.com'
+        subtext: 'I try to write as often as I can'
       },
       recentBlog: {
         header: 'recent blogs',
@@ -120,7 +120,7 @@ export default {
       },
       uses: {
         header: 'uses',
-        subtext: 'a quick summary of what I use on a daily basis to code and some codestats.net flex'
+        subtext: 'some codestats.net flex'
       },
       projects: {
         header: 'projects',
