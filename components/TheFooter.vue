90ae5c1362941c5e55232efb9eb9c0b259c0f8ee
Malloy Okon <mokut.okon4@gmail.com>
2022-02-07 16:08:10 +0100

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -32,16 +32,7 @@
           
         </div>
         <p class="mt-8 text-sm leading-6 text-gray-400 md:mt-0 md:order-1">
-          {{ $config.name }} <span class="ceator-preview hover:text-hot-pink text-gray-300">
-          <a class="hidden md:inline-block" rel="noreferrer" target="_blank" href="https://github.com/karngyan/karngyan.com">
-          <creator-preview
-            :avatar="avatar"
-            :name="name"
-            :description="description"
-            :content="content" :mode="mode" />
-          </a>
-          
-          </span>
+          {{ $config.name }} 
         </p>
       </div>
     </div>
